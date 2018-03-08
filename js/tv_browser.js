e6d6e1c92ffea08b2a3a333a74cecfbd2b0d6ad6
wenhao <wenhao@freeloops.local>
2018-03-08 10:31:16 +0800

    submission

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,22 +1,19 @@
-// API Docs at:
-// http://www.tvmaze.com/api
-
 var getShowSelect = document.getElementById("show-select");
+var getShowDetail = document.getElementById("show-detail");
+var response = null;
 
 document.addEventListener('DOMContentLoaded', function() {
-  //hide getShowSelect by default
   getShowSelect.style.visibility="hidden";
-
-  //when user submit search for tv show
+  //get search value
   var submitButton = document.getElementsByTagName('input')[1];
   submitButton.addEventListener('click', doSubmit);
+  //Add an event listener “change” to the select button. 
+  getShowSelect.addEventListener('change',searchDetails);
 });
 
-//search for show
 function doSubmit (event){
 	event.preventDefault();
-	var input = document.getElementById("show-search");
-	var title = input.value;
+	var title = document.getElementById("show-search").value;
 	httpRequest(title);
 	getShowSelect.style.visibility="visible";
 	var firstOption = document.getElementsByTagName("option")[0];
@@ -27,44 +24,54 @@ function doSubmit (event){
 function httpRequest(title){
 	var request = new XMLHttpRequest();
 
-	var responseHandler = function() {
-		console.log("response text", this.responseText);
-  		var response = JSON.parse( this.responseText );
-  		console.log(response);
-  		display(response);
+	var responseNames = function() {
+		//Send a new request to api. Push the result into a global variable for later use
+  		response = JSON.parse(this.responseText);
+  		//console.log(response);
+  		searchShow(response);
 	};
-
-	var requestFailed = function(){
-		console.log("response text", this.responseText);
-		console.log("status text", this.statusText);
-		console.log("status code", this.status);
-	};
-
-	// listen for the request response
-	request.addEventListener("load", responseHandler);
-	request.addEventListener("error", requestFailed);
-
-	// ready the system by calling open, and specifying the url
+	request.addEventListener("load", responseNames);
 	var link = "http://api.tvmaze.com/search/shows?q=" + title;
-	//var link ="http://www.tvmaze.com/search?q=test";
 	request.open("GET", link);
-
-	// send the request
 	request.send();
 }
 
-function display(response){
+function searchShow(response){
+	//Loop through result to get all names. Create a new option element for name.
 	for(var i=0;i<response.length;i++){
 		var showName = response[i]["show"]["name"];
-		//console.log(showName);
-		var image = "";
-		if(response[i]["show"]["image"] == null){
-			image = "<No image link found>";
-		}else{
-			image = response[i]["show"]["image"]["original"];
-		}
 		var createOption = document.createElement("option");
 		getShowSelect.appendChild(createOption);
-		createOption.textContent = showName + "<" + image + ">";
+		createOption.textContent = showName;
+
+		//get image
+		if(response[i]["show"]["image"] === null){
+		var createImg = document.createElement("p");
+		createImg.textContent="<No image link found>";
+		}
+		else{
+			var createImg = document.createElement("img");
+			createImg.setAttribute("src",response[i]["show"]["image"]["medium"]);
+		}
+		getShowDetail.appendChild(createImg);
+		createImg.setAttribute("class","image");
+		createImg.style.display="none";
 	}
 }
+
+function searchDetails(event){
+	event.preventDefault();
+	var input = document.getElementById("show-select").selectedIndex;
+	//console.log(input);
+
+	var images = document.getElementsByClassName("image");
+	for(var i=0;i<images.length;i++){
+		//console.log(images[i]);
+		images[i].style.display="none";
+	}
+	
+	var showImage = images[input-1];
+	//console.log(showImage);
+	showImage.style.display="block";
+}
+
