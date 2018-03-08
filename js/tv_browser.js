b8c2045b798e731e651f02924661adb49aec2301
zw-zack <zhengwin94@hotmail.com>
2018-03-08 09:20:46 +0800

    incompleted

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,62 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+
+window.onload = function(){
+
+
+	function hideSS() {
+		document.getElementById("show-select").style.display = "none";
+	}
+
+	hideSS();
+
+	function showSS(event) {
+		event.preventDefault();
+
+		document.getElementById("show-select").style.display = "block";
+	}
+
+	document.querySelectorAll('input')[1].addEventListener('click', showSS);
+
+	var responseHandler = function() {
+		console.log("response text", this.responseText);
+		var response = JSON.parse( this.responseText );
+		console.log( response );
+
+		var moviesList = [];
+		//for(var i = 0 ; i < response.length; i++){
+		response.forEach(function(abc){
+			moviesList.push(abc.show.name);
+			var showDetail = document.getElementById("show-select");
+			var addedOption = document.createElement("option");
+			addedOption.textContent = String(abc.show.name);
+			showDetail.appendChild(addedOption);
+		})
+
+		/*{
+			moviesList.push(response[i].show.name);
+			var showDetail = document.getElementById("show-select");
+			var addedOption = document.createElement("option");
+			addedOption.textContent = String(response[i].show.name);
+			showDetail.appendChild(addedOption);
+		}
+*/
+		console.log(moviesList);
+	};
+
+	var doSubmit = function(event){
+		event.preventDefault();
+		var input = document.querySelectorAll('input')[0];
+		var url = "http://api.tvmaze.com/search/shows?q=" + String(input.value);
+		var request = new XMLHttpRequest();
+
+		request.addEventListener("load", responseHandler);
+
+		request.open("GET", url);
+
+		request.send();
+	};
+
+	document.querySelectorAll('input')[1].addEventListener('click', doSubmit);
+}
