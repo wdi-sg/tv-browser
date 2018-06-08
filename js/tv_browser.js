03110f8698920dec21c812b9f04130161383813a
Lee Wen Yao <peterleewenyao@gmail.com>
2018-06-08 19:12:28 +0800

    check for matching shows

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,41 +1,48 @@
 // API Docs at:
 // http://www.tvmaze.com/api
-
 //var optsArr = [];
+
 var responseHandler = function() {
   console.log("response text", this.responseText);
-  var response = JSON.parse( this.responseText );
+  var respOfShows = JSON.parse( this.responseTexts );
   console.log( response );
 };
 
-var requestFailed = function(){
+var requestFailed = function(){ //function to call if request fails
   console.log("response text", this.responseText);
   console.log("status text", this.statusText);
   console.log("status code", this.status);
 };
 
 var linkToPage = function(){
-	var request = new XMLHttpRequest();
+	var request = new XMLHttpRequest(); //create a new request
 	request.addEventListener("load", responseHandler); 
-	request.open("GET", "url");
+	request.open("GET", "url");//specify the url and opening to send and receive
 	request.addEventListener("error", requestFailed);
-	request.send();
+	request.send(); //to send request
 	 };
 
+var firstOpt = document.getElementById('firstOpt');
 var userInput = document.getElementsByTagName('input')[0];
 var selection = document.getElementById('show-select');
 var addAnewOpt = function() {
-	var newOpt = document.createElement('option');
-	newOpt.setAttribute("value", "");
-	newOpt.textContent = userInput.value
-	selection.appendChild(newOpt);
-	//newOpt.addEventListener('click', linkToPage);
+	var actualShows = document.createElement('option');
+	actualShows.setAttribute("value", "");
+	firstOpt.innerText = "Shows matching " +userInput.value+ "...";
+	//if userInput has part of the name of the respOfShows
+	respOfShows.forEach(function(element){
+		var match = element.includes(userInput);
+		if (match == true) {
+			actualShows.textContent = respOfShows;
+			selection.appendChild(actualShows);
+		}
+});
 }
 var submitBtn = document.getElementsByTagName('button')[0];
-submitBtn.addEventListener('click', addAnewOpt)
+submitBtn.addEventListener('click', addAnewOpt);
 
 
-
+//actualShows.addEventListener('click', linkToPage);
 
 
 
