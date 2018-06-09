5b935b9f638305b2894fc3aa3852f1a7f50e9442
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-06-09 19:44:13 +0800

    able to get search results AJAX

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,41 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+// API Docs at:
+// http://www.tvmaze.com/api
+
+ var doSubmit = function(event){
+
+  var input = document.querySelector('#show-search');
+  var url = 'http://api.tvmaze.com/search/shows?q=' + input.value;
+  var optionList = document.querySelector('#show-select') 
+
+  var responseHandler = function() {
+    console.log("response text", this.responseText);
+    var response = JSON.parse( this.responseText );
+    console.log( response );
+    for (var i = 0; i < response.length; i++) {
+    	console.log(response[i].show.name)
+      // optionList.createEleme = <option>response[i].show.name</option>.
+      console.log('hi')
+    }
+  };
+ 
+  // make a new request
+  var request = new XMLHttpRequest();
+  // listen for the request response
+  request.addEventListener("load", responseHandler);
+  // ready the system by calling open, and specifying the url
+  request.open("GET", url);
+  // send the request
+  request.send();
+
+  var requestFailed = function(){
+    console.log("response text", this.responseText);
+    console.log("status text", this.statusText);
+    console.log("status code", this.status);
+  };
+  request.addEventListener("error", requestFailed);
+
+};
+document.querySelector('#submit').addEventListener('click', doSubmit);
