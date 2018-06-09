8d46ed15ba1edf7b688857f109d713768676b3a1
adna <adna@adna.com>
2018-06-10 00:13:10 +0800

    Move request callback functions definitions out of event listeners to avoid multiple redefinitions

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -20,6 +20,7 @@ container.appendChild(summary);
 body.appendChild(container);
 
 var input = document.getElementById("show-search");
+var inputValue = "";
 var button = document.getElementById("submitButton");
 var select = document.getElementById("show-select");
 var tvMazeSearchEndpoint = "http://api.tvmaze.com/search/shows?q=";
@@ -33,46 +34,47 @@ var clearAll = function() {
     summary.innerHTML = "";
 }
 
+var requestFailed = function() {
+    console.log("Failed");
+}
+
+var getMovies = function()  {
+    var moviesArray = JSON.parse(this.responseText);
+
+    // clear the previous movie object
+    movies = {};
+
+    // transform the moviesArray into an oject for easy access
+    // each movie name becomes a key, and the entire info like {name: girl movie, summary: some plot, etc} becomes the object (value)
+    // movie[key] = value
+    moviesArray.forEach(function(movie) {
+        movies[movie.show.name] = movie;
+    })
+
+    var defaultOption = document.createElement("option");
+    defaultOption.textContent = "Shows matching " + inputValue + "...";
+    select.appendChild(defaultOption);
+    select.style.display = "flex";
+
+    moviesArray.forEach(function(movie) {
+        var option = document.createElement("option");
+        option.textContent = movie.show.name
+        select.appendChild(option);
+    })
+}
+
+// put after requestFailed and getMovies are defined
+var request = new XMLHttpRequest();
+request.addEventListener("error", requestFailed);
+request.addEventListener("load", getMovies);
+
 // Event Listener to search movies with keyword
 var searchMatches = function() {
-    var inputValue = input.value;
+    inputValue = input.value;
 
     clearAll();
 
-    var requestFailed = function() {
-        console.log("Failed");
-    }
-
-    var getMovies = function()  {
-
-        var moviesArray = JSON.parse(this.responseText);
-
-        // clear the previous movie object
-        movies = {};
-
-        // transform the moviesArray into an oject for easy access
-        // each movie name becomes a key, and the entire info like {name: girl movie, summary: some plot, etc} becomes the object (value)
-        // movie[key] = value
-        moviesArray.forEach(function(movie) {
-            movies[movie.show.name] = movie;
-        })
-
-        var defaultOption = document.createElement("option");
-        defaultOption.textContent = "Shows matching " + inputValue + "...";
-        select.appendChild(defaultOption);
-        select.style.display = "flex";
-
-        moviesArray.forEach(function(movie) {
-            var option = document.createElement("option");
-            option.textContent = movie.show.name
-            select.appendChild(option);
-        })
-    }
-
-    var request = new XMLHttpRequest();
     request.open("GET", tvMazeSearchEndpoint + inputValue);
-    request.addEventListener("error", requestFailed);
-    request.addEventListener("load", getMovies);
     request.send();
 }
 
