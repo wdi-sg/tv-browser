d1d50efa2de0c6faec08dab37c6037dff2abefd6
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-06-10 01:00:47 +0800

    in the middle of further 2

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -3,20 +3,39 @@
 
  var doSubmit = function(event){
 
+  // varibale which selects the input element by its ID 		
   var input = document.querySelector('#show-search');
+
+  //variable which retuns the URL of the user's input
   var url = 'http://api.tvmaze.com/search/shows?q=' + input.value;
+
+  //variable that selecets the options element by its ID
   var optionList = document.querySelector('#show-select') 
 
   var responseHandler = function() {
+
+  	//response text returns a string
     console.log("response text", this.responseText);
+    
+    // converts string and returns us an array.
     var response = JSON.parse( this.responseText );
     console.log( response );
+    
     for (var i = 0; i < response.length; i++) {
     	console.log(response[i].show.name)
-      var option = document.createElement('OPTION')
-      var optionName = document.createTextNode(response[i].show.name);
-      option.appendChild(optionName);
-      optionList.appendChild(option);
+
+    	//create an option tag for each item in array retuned
+     	var option = document.createElement('OPTION')
+     	// each option tag is given a specific id(thetvdb number). this is so it can be accessed later
+     	option.setAttribute('id', ("http://api.tvmaze.com/lookup/shows?thetvdb=" + response[i].show.externals.thetvdb))
+     	option.setAttribute('class', 'clickable')
+     	
+     	//New variable to fill up words option tag. It is appended to the option tag
+     	var optionName = document.createTextNode(response[i].show.name);
+     	option.appendChild(optionName);
+     	//option tags are then appended to its rightful parentnode
+     	optionList.appendChild(option);
+     	console.log(response[i].show.externals.thetvdb) 
     }
   };
  
@@ -38,3 +57,25 @@
 
 };
 document.querySelector('#submit').addEventListener('click', doSubmit);
+
+
+// further 2
+
+var clickOption = function(event){
+
+	//variable to create an array of the options listed
+	var optionNames = document.querySelectorAll('.clickable');
+
+	// for loop to add a click event listener to each option name
+	for (var i = 0; i < optionNames.length; i++) {
+		optionNames[i].addEventListener('click', function(){
+			var url = optionNames[i].id
+			console.log(url.responseText);
+			
+		})
+	}
+}
+
+
+
+
