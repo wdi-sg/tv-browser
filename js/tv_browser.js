0a1889bc60ad3958ac3edf4f7134158edaeb8040
jodich <jodi_choo@hotmail.com>
2018-06-09 14:07:14 +0800

    mvp done

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,61 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+window.onload = function() {
+
+function responseHandler() {
+	var data = JSON.parse(this.responseText)
+	
+	var displayArea = document.querySelector('#show-detail')
+	
+	console.log(data)
+	for (var i = 0; i < data.length; i++) {
+		var title = document.createElement('h2');
+		var summary = document.createElement('div');
+		var image = document.createElement('img');
+
+		if (data[i].show.image === null) {
+			// do nothing
+		} else {
+			image.setAttribute("src", data[i].show.image.medium)
+		}
+
+		var showName = data[i].show.name
+		var showSummary = data[i].show.summary
+
+		title.innerHTML = showName
+		summary.innerHTML = showSummary
+
+		displayArea.appendChild(title)
+		displayArea.appendChild(image)
+		displayArea.append(summary)
+
+	}
+}
+
+function errorHandler() {
+	console.log(this.statusText)
+}
+
+
+function newRequest(url) {
+	var request = new XMLHttpRequest;
+	request.addEventListener('load', responseHandler);
+	request.addEventListener('error', errorHandler);
+	request.open('GET', url);
+	request.send();
+}
+
+function onClick() {
+	var searchQuery = document.querySelector('#show-search').value
+	var url = "http://api.tvmaze.com/search/shows?q=" + encodeURIComponent(searchQuery);
+	newRequest(url);
+}
+
+
+document.querySelector('button').addEventListener('click', onClick);
+
+
+
+
+
+}
