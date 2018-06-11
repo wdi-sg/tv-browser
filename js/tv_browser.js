eb4612af930268c3e96846fd027481750d7a58c4
Phang Chiew <drillaxholic@Phang-Chiews-MacBook-Pro.local>
2018-06-11 14:08:42 +0800

    complete untill clear part

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,17 +1,35 @@
 // API Docs at:
 // http://www.tvmaze.com/api
 var btn=document.getElementById('submit');
-btn.addEventListener('click',check);
+var select = document.getElementById('show-select');
+var movies;
+
+var clearAll = function() {
+    select.innerHTML = "";                 //clear when diff input
+    selectedMovie.textContent = "";			//clear title
+    createImageDiv.style.display = "none";	//clear img
+    createSummaryDiv.innerHTML = "";		//clear summary
+}
 
 var responseHandler = function() {
  	// console.log("response text", this.responseText);
- 	var response = JSON.parse( this.responseText );
- 	console.log(response);
- 	// calling out the title
- 	title(response);
- 	//number of total movies
- 	numTitle(response);
-};
+ 	var moviesArray = JSON.parse( this.responseText );
+
+	movies = {};
+	//making the key:value for each movie
+	moviesArray.forEach(function(movie){
+		movies[movie.show.name]= movie
+	})
+
+ 	//getting the title of the movie out in each search
+	moviesArray.forEach(function(movie) {
+		var option = document.createElement('option');
+		option.textContent = movie.show.name;
+		var select = document.getElementById('show-select');
+		select.appendChild(option);
+	});
+}
+
 var requestFailed = function(){
  	console.log("response text", this.responseText);
  	console.log("status text", this.statusText);
@@ -24,45 +42,56 @@ function sendxhr(url){
 	request.addEventListener("error", requestFailed);// run an error function if an error occurs
 	request.open("GET", url); // ready the system by calling open, and specifying the url
 	request.send(); // send the request
+
+
 }
 
 function check(){
 	var url='http://api.tvmaze.com/search/shows?q=' + document.getElementById("show-search").value;
-	sendxhr(url)
+	sendxhr(url);
 }
 
+btn.addEventListener('click',check);
 
-//getting the title of the movie out in each search
-function title(access){
-	for(i=0;i<access.length;i++){
-		var movie = access[i].show.name;
-		// console.log('The movie is '+movie);
-		// appending the movie into the HTML from script
-		// create option tag
-		var create = document.createElement('option');
-		create.setAttribute('value','movies');
-		//create a text node
-		var disMovies = document.createTextNode(movie);
-		//append the option to the text node
-		create.appendChild(disMovies);
-		var selectedMovie = document.getElementById("show-select").appendChild(create);
-		console.log(selectedMovie); //shows the option
-		var writeUp = access[i].show.summary;
-		// console.log('Title of the movie: ' + movie );
-		// console.log('Summary of the movie: ' + writeUp);
-	};
-};
 
 function setClick(){
-	var x =document.getElementById('show-select');
-	var i = x.selectedIndex;
-    document.getElementById("item").textContent = x.options[i].text;
+	// var x =document.getElementById('show-select');
+	// var i = x.selectedIndex; //selectIndex is a special word.
+ //    document.getElementById("title").innerHTML = x.options[i].text;
+    var selectedMovie = this.value;
+    var selectedMovieName = movies[selectedMovie];
+	var getTitle = document.getElementById('title');
+	getTitle.textContent = selectedMovie;
+
+	var createImageDiv = document.createElement('div');
+	var createImageTag = document.createElement('img');  //create element
+	// in case some movie has no images
+    if (selectedMovieName.show.image) {
+        createImageDiv.style.display = "flex";
+        createImageTag.src = selectedMovieName.show.image.medium;
+    } else {
+        createImageDiv.style.display = "none";
+    }
+	var showDetail = document.getElementById('show-detail'); //append element to group
+	createImageDiv.appendChild(createImageTag);
+	showDetail.appendChild(createImageDiv); //appending to the showDetail group
+
+	var createSummaryDiv=document.createElement('div');
+	var writeUp = selectedMovieName.show.summary;
+	createSummaryDiv.innerHTML=writeUp;
+	showDetail.appendChild(createSummaryDiv); //appending to the showDetail group
 }
+
+	// console.log('Title of the movie: ' + movie );
+	// console.log('Summary of the movie: ' + writeUp);	
+
+select.addEventListener('change',setClick);
+
 //the number of titles each search has
-function numTitle(avail){
-	var numOfMovies = avail.length;
-	console.log('The number of movies ' + numOfMovies);
-}
+// function numTitle(avail){
+// 	var numOfMovies = avail.length;
+// 	console.log('The number of movies ' + numOfMovies);
+// }
 
 
 // #####Bryan's understanding and trial to AJAX
