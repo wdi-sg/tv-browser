60f56e4ca6f3f6d04f6e22098f103893ad03eeb5
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-13 16:05:47 +0800

    Implement MVP - search and display results

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,30 @@
-// API Docs at:
-// http://www.tvmaze.com/api
+window.onload = function() {
+    var button = document.getElementsByTagName('button')[0]
+    var board = document.getElementById('search-form')
+
+    function retrieveResults(event) {
+        var inputValue = document.getElementsByTagName('input')[0].value
+        var responseHandler = function() {
+            var responseObject = JSON.parse(this.responseText)
+            var result = []
+            for (i in responseObject) {
+                result.push(responseObject[i].show.name)
+                var display = document.createElement('p')
+                display.textContent = result[i]
+                board.appendChild(display)
+            }
+            console.log("status text", this.statusText)
+            console.log("status code", this.status)
+            console.log(inputValue)
+
+        }
+
+        var request = new XMLHttpRequest()
+        request.addEventListener("load", responseHandler)
+        request.open("GET", `http://api.tvmaze.com/search/shows?q=${inputValue}`)
+        request.send()
+
+
+    }
+    button.addEventListener('click', retrieveResults)
+}
