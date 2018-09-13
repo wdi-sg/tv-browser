3ead9775c489d654af35c2bfe4cc3e3162a7f3c8
ChloeLiang <nliangxin@gmail.com>
2018-09-13 17:32:21 +0800

    Fill in the select element

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -4,7 +4,9 @@ window.onload = function () {
   var baseUrl = 'http://api.tvmaze.com/search/shows?q=';
   var detail = document.getElementById('show-detail');
   var input = document.getElementById('show-search');
+  var select = document.getElementById('show-select');
   var button = document.querySelector('button');
+  var responseShows;
 
   var displayShowDetail = function (show) {
     var container = document.createElement('div');
@@ -23,11 +25,39 @@ window.onload = function () {
     detail.appendChild(container);
   };
 
+  var getShow = function (showName) {
+    var i;
+
+    for (i = 0; i < responseShows.length; i++) {
+      if (responseShows[i].show.name === showName) {
+        return responseShows[i];
+      }
+    }
+  };
+
+  var clearDetail = function () {
+    while (detail.firstChild) {
+      detail.removeChild(detail.firstChild);
+    }
+  };
+
+  var addOption = function (show) {
+    var option = document.createElement('option');
+
+    option.value = show.show.name;
+    option.innerHTML = show.show.name;
+    select.appendChild(option);
+  };
+
   var searchShowHandler = function () {
-    var shows = JSON.parse(this.responseText);
-    shows.forEach(function (show) {
+    responseShows = JSON.parse(this.responseText);
+
+    responseShows.forEach(function (show) {
       displayShowDetail(show);
+      addOption(show);
     });
+
+    select.options[0].innerHTML = 'Shows matching ' + input.value + '...';
   };
 
   var searchShow = function () {
@@ -37,5 +67,15 @@ window.onload = function () {
     request.send();
   };
 
-  button.addEventListener('click', searchShow);
+  var setup = function () {
+    button.addEventListener('click', searchShow);
+
+    select.addEventListener('change', function () {
+      clearDetail();
+      displayShowDetail(getShow(this.value));
+    });
+  };
+
+  setup();
+
 };
