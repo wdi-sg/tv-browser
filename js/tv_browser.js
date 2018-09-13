d2fc118577897200806b2da4432eddc89ee22acf
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-13 16:24:47 +0800

    added a get request for search term

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,31 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+window.onload = function(){
+    // what to do when we recieve the request
+    var responseHandler = function() {
+      console.log("response text", this.responseText);
+      var response = JSON.parse( this.responseText );
+    };
+
+    var doSubmit = function(){
+        var input = document.querySelector("#show-search");
+        var url = " http://api.tvmaze.com/search/shows?q=" + input.value;
+
+        //console.log( response )
+        // make a new request
+        var request = new XMLHttpRequest();
+
+        // listen for the request response
+        request.addEventListener("load", responseHandler);
+
+        // ready the system by calling open, and specifying the url
+        request.open("GET",  url);
+        // send the request
+        request.send();
+    }
+
+    document.querySelector("#submit").addEventListener('click', doSubmit);
+}
+
+
