551b02d548db80d114b73c5c6428fbedea7810f8
weslie1234 <weslie1234@gmail.com>
2018-09-14 00:24:40 +0800

    Completed search + showing the list

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,31 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+var submit = document.getElementById("submit_button");
+submit.addEventListener("click", function() {
+    var output = document.getElementById("show-search").value;
+    console.log(output);
+
+    var responseHandler = function() {
+	  console.log("response text", this.responseText);
+	  var response = JSON.parse(this.responseText);
+	  var select = document.getElementById("show-select");
+	  for (var i = 0; i < response.length; i++) {
+	  	var out = response[i].show.name;
+	  	var opt = document.createElement("option");
+	  	opt.textContent = out;
+	  	select.appendChild(opt);
+	  }
+	  console.log("status text", this.statusText);
+	  console.log("status code", this.status);
+	};
+
+	var request = new XMLHttpRequest();
+	request.addEventListener("load", responseHandler);
+	request.open("GET", "http://api.tvmaze.com/search/shows?q=" + output);
+	request.send();
+});
+
+
+
+
