8da5a03141707fa4c88dc951ab63a259e4534ad4
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-13 18:51:22 +0800

    added a comment block that allows show names to be displayed on the webpage after searching

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -7,14 +7,36 @@ window.onload = function(){
       console.log("response text", this.responseText);
       var response = JSON.parse( this.responseText );
       var arrayShowName = [];
+
       for (var i = 0; i < response.length; i++){
         var getDiv = document.getElementById("show-select");
-        var getDivp = document.createElement("option");
-        getDivp.innerHTML = response[i].show.name;
-        getDiv.appendChild(getDivp);
-
+        var getOption = document.createElement("option");
+        getOption.innerHTML = response[i].show.name;
+        getDiv.appendChild(getOption);
+        //Display show name on the front page below the search button
+        // var showDetailId = document.getElementById(“show-detail”);
+        // var createPTag = document.createElement(“P”);
+        // createPTag.textContent = response[i].show.name;
+        // showDetailId.appendChild(createPTag);
       }
+
+      document.querySelectorAll('option').addEventListener('change', function(){
+        console.log()
+
+        // var url2 = "http://api.tvmaze.com/singlesearch/shows?" + this.value;
+        // var request2 = new XMLHttpRequest();
+        // request2.open("GET", url2);
+        // request2.send();
+
+      })
+
+
+
     };
+
+    // var responseHandler2 = function(){
+
+    // }
 
 
 
@@ -35,6 +57,11 @@ window.onload = function(){
         // send the request
         request.send();
     }
+
+    // var makeAnotherRequest = function(){
+
+
+    // }
 
     document.querySelector("#submit").addEventListener('click', doSubmit);
 }
