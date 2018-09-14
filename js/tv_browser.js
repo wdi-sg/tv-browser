990afa8836c2869d9ff3f45ab32fe7b95bd91fbf
abdulhakimsg <abdulhakimchi@gmail.com>
2018-09-14 10:38:55 +0800

    solved

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,16 +1,18 @@
 // API Docs at:
 // http://www.tvmaze.com/api
-
 window.onload = function(){
     let url = `http://api.tvmaze.com/search/shows?q=`
+    var selection = document.getElementById('show-select')
+    var girlArray;
+
 
     //if press Submit give response
     var doSubmit = function(event){
         var input = document.getElementById('show-search'); var link = url + input.value
+        request.addEventListener("load", responseHandler)
         request.open("GET", link)
         request.send()
         console.log(link)
-    return doSubmit
     }
 
     //link click on button to a function
@@ -21,59 +23,45 @@ window.onload = function(){
 
     //function to parse into JSON Object
     var responseHandler = function(){
-        var response = JSON.parse(this.responseText)
-        console.log(response)
-        var girlArray = response
+
+        console.log("status code: ", this.status);
+        girlArray = JSON.parse(this.responseText)
+        console.log(girlArray)
 
         girlArray.forEach(function(element){
-            var selection = document.getElementById('show-select')
             var option = document.createElement('option')
             selection.appendChild(option)
+            option.setAttribute("value",element.show.name)
             option.innerText= element.show.name
-            return option
         })
 
-    //if press select show give response
-    var clickedOption = function(event){
-        var response = JSON.parse(this.responseText)
+    }
+
+    //run code for details
+    var giveDetails = function(){
+
         var showDetail = document.getElementById('show-detail')
-        var title = document.createElement('h1')
-        showDetail.appendChild(title)
-        var para = document.createElement('p')
-        showDetail.appendChild(para)
-        var image = document.createElement('img')
-        showDetail.appendChild(image)
-
-        title.innerHTML = this.show.name
-        image.setAttribute('src' , this.show.image.medium )
-        para.innerHTML = this.show.summary
-        console.log(option)
-    }
-
-    //link click on show select
-    document.getElementById('show-select').addEventListener('onchange' , clickedOption)
     
-    //make request
-    var request = new XMLHttpRequest()
+        while(showDetail.firstChild){
+            showDetail.removeChild(showDetail.firstChild)
+        }
 
-    // //run code for details
-    // var responseHandler = function(){
-    //     var response = JSON.parse(this.responseText)
-    //     var showDetail = document.getElementById('show-detail')
-    //     var title = document.createElement('h1')
-    //     showDetail.appendChild(title)
-    //     var para = document.createElement('p')
-    //     showDetail.appendChild(para)
-    //     var image = document.createElement('img')
-    //     showDetail.appendChild(image)
+            var title = document.createElement('h1')
+            showDetail.appendChild(title)
+            var para = document.createElement('p')
+            showDetail.appendChild(para)
+            var image = document.createElement('img')
+            showDetail.appendChild(image)
 
-    //     title.innerHTML = this.show.name
-    //     image.setAttribute('src' , this.show.image.medium )
-    //     para.innerHTML = this.show.summary
-    //     return responseHandler
-    // }
-    
-    }
-    //function to load
-    request.addEventListener("load", responseHandler)
+            title.innerHTML = this.value
+            console.log(this.value)
+
+            image.setAttribute("src", girlArray[this.options.selectedIndex -1].show.image.medium)
+            console.log(image.src);
+
+            para.innerHTML = girlArray[this.options.selectedIndex -1].show.summary
+            console.log(para)
+        }
+    //function to load 
+    selection.addEventListener('change' , giveDetails)
 }
