4da0f68e8cfca388fcb106e9ea58048ec456b8c0
kingcashthefifth <kingcashthefifth@gmail.com>
2018-12-06 21:57:07 +0800

    managed to get ajax request, parse JSON, append DOM to select options

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -2,18 +2,33 @@
 // http://www.tvmaze.com/api
 window.onload = function() {
 
-    var responseHandler = function() {
-        console.log("response text", this.resopnseText);
+    function responseFailed() {
+        console.log("response text", this.responseText);
         console.log("status text", this.statusText);
         console.log("status code", this.status);
+    }
+
+    function responseHandler(event) {
+        var getObject = JSON.parse(this.responseText);
+        var showNameResult = [];
+
+        for (key in getObject) {
+            showNameResult.push(getObject[key].show.name);
+            var option = document.createElement('option');
+            option.innerText = showNameResult[key];
+            var getSelect = document.querySelector("#show-select");
+            getSelect.appendChild(option);
+        };
+
     };
 
-    var doSubmit = function(event) {
+    function doSubmit(event) {
         var input = document.querySelector("#show-search");
         var url = input.value;
 
         var request = new XMLHttpRequest();
         request.addEventListener("load", responseHandler);
+        request.addEventListener("error", responseFailed);
         request.open("GET", "http://api.tvmaze.com/search/shows?q="+url);
         request.send();
     };
