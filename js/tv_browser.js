d1601e383a06b4cbc24706473fd22733119e7718
kingcashthefifth <kingcashthefifth@gmail.com>
2018-12-06 20:53:59 +0800

    completed MVP

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,21 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+window.onload = function() {
+
+    var responseHandler = function() {
+        console.log("response text", this.resopnseText);
+        console.log("status text", this.statusText);
+        console.log("status code", this.status);
+    };
+
+    var doSubmit = function(event) {
+        var input = document.querySelector("#show-search");
+        var url = input.value;
+
+        var request = new XMLHttpRequest();
+        request.addEventListener("load", responseHandler);
+        request.open("GET", "http://api.tvmaze.com/search/shows?q="+url);
+        request.send();
+    };
+document.querySelector("#submitButton").addEventListener("click", doSubmit);
+};
