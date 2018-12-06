f1bf8eb20dcaf4f4b49030ab3308e09d55413e03
Ishak <ishakbhn@gmail.com>
2018-12-06 23:09:54 +0800

    add eventlistener for search, get request and append search name to DOM dropdown.

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,39 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+//console.log is for checking purposes
+
+var responseHandler = function(){
+    //convert string to JSON object
+    var response = JSON.parse(this.response);
+
+    //type of reponse if an array of object
+    for(i=0; i < response.length; i++){
+        //get all array with object name
+        var name = response[i].show.name;
+        //create element option with search names, append to drop down
+        var option = document.createElement("option")
+        option.innerHTML = name;
+        document.querySelector("#show-select").appendChild(option);
+    }
+}
+
+
+var getSearch = function (){
+
+    var searchWord = this.value;
+
+    var request = new XMLHttpRequest();
+
+    request.addEventListener("load",responseHandler);
+
+    request.open("GET", " http://api.tvmaze.com/search/shows?q=" + searchWord)
+
+    request.send();
+
+}
+
+window.onload = function(){
+    //get the search input and do function.
+    document.querySelector("#show-search").
+        addEventListener("search", getSearch);
+}
