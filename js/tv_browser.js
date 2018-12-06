4106d7126c0fa2f0524a7e269b1e136ceda385fe
sean chan <sean@seans-MacBook-Air.local>
2018-12-06 22:21:09 +0800

    load json api

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,33 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+window.onload = function() {
+    // what to do when we recieve the request
+
+var responseHandler = function() {
+  console.log("response text", this.responseText);
+  console.log("status text", this.statusText);
+  console.log("status code", this.status);
+};
+
+
+    var doRequest = function(){
+
+    var userInput = document.querySelector('#show-search');
+
+    var id = userInput.value;
+
+    var request = new XMLHttpRequest();
+
+    // listen for the request response
+    request.addEventListener("load", responseHandler);
+
+    request.open("GET", "http://www.tvmaze.com/search?q="+id);
+
+
+    request.send();
+
+}
+
+    document.querySelector('#submit').addEventListener('click', doRequest);
+}
