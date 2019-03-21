7fabcfafadaca8eff937826b25a72e4e106333af
seeo <aragonlotr5@gmail.com>
2019-03-22 00:56:58 +0800

    added in the ability to show tvshow name, medium image, ratings and summary under the show-detail id div tag. But the problem I am getting is that everything new gets appended below the older children.

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -3,10 +3,17 @@
 
 //when user input a word into the #showsearch "searchbar", use ajax to connect to TV Maza while using the API to "GET" a list of relevant TV show names.
 //These should just be names that are presented onto the
+
+var response=[];
+
 var responseHandler = function(){
   console.log("response text", this.responseText);
-  var response = JSON.parse( this.responseText );
+  response = JSON.parse( this.responseText );
   console.log(response);
+  listShows();
+}
+
+var listShows = function(){
   for (var i = 0; i < response.length; i++){
     var option = document.createElement("option");
     option.innerText = response[i].show.name;
@@ -21,7 +28,6 @@ var responseHandler = function(){
 var doSubmit = function(event){
   var input = document.querySelector('#show-search');
   var searchTerm = input.value;
-
   // var searchUrl = "http://api.tvmaze.com/search/shows?q=" + searchTerm
   // make a new request
   var request = new XMLHttpRequest();
@@ -38,3 +44,49 @@ document.querySelector('#submit').addEventListener('click', doSubmit);
 //build a for loop out to handle looping of the array of objects that is generated
 //can we use a forEach loop, instead of a for loop?
 //show.name
+
+
+var clickOption = function(event){
+  var showId = document.getElementById("show-select").value;
+    var request = new XMLHttpRequest();
+    // listen for the request response
+    request.addEventListener("load", anotherHandler);
+    // ready the system by calling open, and specifying the url
+    request.open("GET", "http://api.tvmaze.com/shows/"+showId);
+    // send the request
+    request.send();
+  }
+
+var anotherHandler = function(){
+  console.log("response text", this.responseText);
+  response = JSON.parse( this.responseText );
+  console.log(response);
+  showDetail();
+
+}
+
+var showDetail = function(){
+
+  var name = document.createElement("h4");
+  name.innerText = response.name
+  document.getElementById("show-detail").appendChild(name);
+
+  var image = document.createElement("img");
+  image.src = response.image.medium;
+  document.getElementById("show-detail").appendChild(image);
+
+  var ratings = document.createElement("h5");
+  ratings.innerText = response.average;
+  document.getElementById("show-detail").appendChild(ratings);
+
+  var summary = document.createElement("p");
+  summary.innerText = response.summary;
+  document.getElementById("show-detail").appendChild(summary);
+}
+
+
+//document.querySelector("#show-select").addEventListener('click',clickOption);
+
+
+//Attach an event listener to the select. When the user selects an option make another AJAX call.
+//Use the response of that AJAX call to render the individual show they requested
