5084038b8da6b4ca15c81303405dea0b3bcf35a0
Chang <ainurlindale@gmail.com>
2019-03-22 09:04:49 +0800

    step 0

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,18 @@
-// API Docs at:
-// http://www.tvmaze.com/api
+
+var url = 'http://api.tvmaze.com/singlesearch/shows?q=';
+var userInput = prompt("what you want");
+var url = url + userInput;
+
+console.log(url);
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', url)
+ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+
+console.log(ourData.summary);
+}
+ourRequest.send();
+
+
+// http://api.tvmaze.com/singlesearch/shows?q=girls
