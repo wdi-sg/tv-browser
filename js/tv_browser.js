09c103fbc7eccdee4c8131397e58fd722292e9a6
calyeap <calvinyeap.h@gmail.com>
2019-03-22 10:23:08 +0800

    incomplete

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,39 @@
-// API Docs at:
-// http://www.tvmaze.com/api
+window.onload = function () {
+  var sendRequest = function (url) {
+    // what to do when we recieve the request
+    var responseHandler = function () {
+      console.log("response text", this.responseText);
+      var response = JSON.parse(this.responseText);
+      console.log("status text", this.statusText);
+      console.log("status code", this.status);
+    };
+
+    var requestFailed = function () {
+      console.log("response text", this.responseText);
+      console.log("status text", this.statusText);
+      console.log("status code", this.status);
+    };
+
+    // make a new request
+    var request = new XMLHttpRequest();
+
+    // listen for errors
+    request.addEventListener("error", requestFailed);
+
+    // listen for the request response
+    request.addEventListener("load", responseHandler);
+
+    // ready the system by calling open, and specifying the url
+    request.open("GET", url);
+
+    // send the request
+    request.send();
+  };
+
+  var doSubmit = function (event) {
+    var input = document.querySelector('#url');
+    var url = input.value;
+    sendRequest(url);
+  };
+
+  document.querySelector('#submit').addEventListener('click', doSubmit);
