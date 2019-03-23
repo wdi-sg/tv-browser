7e1e7346e7c50b7e49b82a4e40afb34e2b1cf54e
Chang <ainurlindale@gmail.com>
2019-03-23 12:10:04 +0800

    achieved mvp with prompts

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -3,14 +3,30 @@ var url = 'http://api.tvmaze.com/singlesearch/shows?q=';
 var userInput = prompt("what you want");
 var url = url + userInput;
 
+var showContainer = document.getElementById("show-detail");
+var showName = document.createElement('h1');
+var showImage = document.createElement('img');
+var showSummary = document.createElement('p');
+
+showContainer.appendChild(showName);
+showContainer.appendChild(showImage);
+showContainer.appendChild(showSummary);
+
 console.log(url);
 
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', url)
 ourRequest.onload = function() {
     var ourData = JSON.parse(ourRequest.responseText);
+    var name = ourData.name;
+    var summary = ourData.summary;
+    var image = ourData.image.medium;
+    console.log(name);
 
-console.log(ourData.summary);
+    showName.innerHTML = name;
+    showImage.setAttribute("src", image);
+    showSummary.innerHTML = summary;
+// showContainer.appendChild(showSummary);
 }
 ourRequest.send();
 
