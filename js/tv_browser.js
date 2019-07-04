32a8bd92e97f59c3ae6f4b50ef294da68f01d859
wilfredloh <lohwilfred19@gmail.com>
2019-07-05 01:43:35 +0800

    added more features

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,15 +1,6 @@
 console.log('running!')
 
 /////////////////////////////////////////////////////////////////////
-//                         GLOBAL VARIABLES
-/////////////////////////////////////////////////////////////////////
-// used to attach to 1st main event listener to start the whole thing
-let submit = document.querySelector('#submit');
-// used to store shows and check if user has made a search or not
-let shows = [];
-let selectOptions = false;
-
-/////////////////////////////////////////////////////////////////////
 //             RUNS WHEN QUERY IS MADE AND RECEIVED           [2]
 /////////////////////////////////////////////////////////////////////
 
@@ -20,43 +11,91 @@ let responseHandler = function() {
   console.log("Status code:", this.status);
 
   let myObject = JSON.parse(this.responseText);
-  setShows(myObject);
+  if (selectOptions === true){
+    displayShow(myObject);
+  } else {
+    setShows(myObject);
+  }
 }
 /////////////////////////////////////////////////////////////////////
 //                 RUNS WHEN USER CLICKS SUBMIT                [1]
 /////////////////////////////////////////////////////////////////////
+let submit = document.querySelector('#submit');
+let showsSearch = '';
 
-let runRequest = function(event){
+let runSearchRequest = function(event){
+    let select = document.querySelector('#show-select');
+    select.style.display = 'block';
     removeOptions();
-
-    let input;
-    let inputValue;
+    let input = document.querySelector('#show-search');
+    showsSearch = input.value;
     // make a new request
     let request = new XMLHttpRequest();
     // listen for the request response
-    // if (selectOptions === true) {
-    //     input = document.querySelector('#show-search');
-    //     inputValue; //make the option into a string???
-    //     request.addEventListener("load", responseHandler);
-    //     request.addEventListener("error", responseHandler);
-
-    //     selectOptions = false;
-    // } else {
-        input = document.querySelector('#show-search');
-        inputValue = input.value;
-        request.addEventListener("load", responseHandler);
-        request.addEventListener("error", responseHandler);
-    // }
+    request.addEventListener("load", responseHandler);
+    request.addEventListener("error", responseHandler);
     // ready the system by calling open, and specifying the url
     // open doesn't open anything??
-    request.open("GET", "http://api.tvmaze.com/search/shows?q="+inputValue);
+    request.open("GET", "http://api.tvmaze.com/search/shows?q="+showsSearch);
     // send the request
     request.send();
-
     input.value = '';
 }
-submit.addEventListener('click', runRequest);
+submit.addEventListener('click', runSearchRequest);
 
+/////////////////////////////////////////////////////////////////////
+//        SETS ALL THE DOM OPTION ELEMENTS FOR SELECT TAG         [3]
+/////////////////////////////////////////////////////////////////////
+let shows = [];
+let selectOptions = false;
+
+let setShows = function (myObject) {
+    let select = document.querySelector("#show-select");
+    let show;
+    for(let i=0; i<myObject.length; i++){
+        show = myObject[i]["show"]["name"];
+        shows.push(show);
+
+        let option = document.createElement('option');
+        option.classList.add('options');
+        option.textContent = shows[i];
+        select.addEventListener('change', runMovieRequest);
+        select.appendChild(option);
+    }
+    selectOptions = true;
+}
+/////////////////////////////////////////////////////////////////////
+//            RUNS WHEN USER CLICKS ON AN OPTION IN SELECT TAG
+/////////////////////////////////////////////////////////////////////
+let showValue = '';
+
+let runMovieRequest = function(event){
+
+    let input = document.querySelector('#show-select');
+    showValue = input.value;
+
+    let request = new XMLHttpRequest();
+    request.addEventListener("load", responseHandler);
+    request.addEventListener("error", responseHandler);
+    request.open("GET", "http://api.tvmaze.com/search/shows?q="+showsSearch);
+    request.send();
+}
+/////////////////////////////////////////////////////////////////////
+//           SETS DOM IN THE 'SHOW DETAIL' DIV (MOVIE IMAGE AND NAME)
+/////////////////////////////////////////////////////////////////////
+let displayShow = function (myObject) {
+    let detail = document.querySelector("#show-detail");
+    let image = document.createElement('img');
+    let title = document.createElement('h2');
+
+    let indexOfShow = shows.indexOf(showValue);
+    title.textContent = showValue;
+    image.src = myObject[indexOfShow]["show"]["image"]["medium"];
+
+    detail.appendChild(title);
+    detail.appendChild(image);
+
+}
 /////////////////////////////////////////////////////////////////////
 //            DELETES ALL OPTIONS IN THE SELECT TAG
 /////////////////////////////////////////////////////////////////////
@@ -69,31 +108,9 @@ let removeOptions = function () {
         }
         shows = [];
     }
+    //make select options false again so if user wants to click a diff search query, they can do so
+    selectOptions = false;
 }
 /////////////////////////////////////////////////////////////////////
-//        SETS ALL THE DOM OPTION ELEMENTS FOR SELECT TAG         [3]
-/////////////////////////////////////////////////////////////////////
-
-let setShows = function (myObject) {
-    let select = document.querySelector("#show-select");
-    let show;
-    for(let i=0; i<myObject.length; i++){
-        show = myObject[i]["show"]["name"];
-        shows.push(show);
-        show;
-        console.log("DATA: Show: "+ show);
-
-        let option = document.createElement('option');
-        option.classList.add('options');
-        option.textContent = shows[i];
-        option.addEventListener('click', function(){
-            // run another AJAX call
-            // get individual show
-        });
-        select.appendChild(option);
-    }
-    selectOptions = true;
-}
-/////////////////////////////////////////////////////////////////////
-//                         FUNCTION TO RUN
+//                         END OF SCRIPT
 /////////////////////////////////////////////////////////////////////
