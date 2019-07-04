038cacd6ea0c688b9e0e83f27f47e044415eb7da
gitwenlei <ngwenlei@gmail.com>
2019-07-04 20:52:54 +0800

    Added for loop to get results and append to select option

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,37 +1,71 @@
 // API Docs at:
 // http://www.tvmaze.com/api
 
+var showId;
+var showName;
+var showImg;
+var showSum;
+
+
+var addShowtoList = function() {
+    // Display the results
+    //console.log(showId);
+
+    // console.log('type:' + (typeof showId));
+    // console.log(showName);
+    // console.log(showImg);
+    // console.log(showSum);
+
+    // assign show.id as <option> "value
+    // create new <option> element with showId as value"
+
+    var results = document.createElement('option');
+    results.value = showId;
+    results.innerHTML = showName;
+
+    var sel = document.querySelector('#show-select');
+
+    sel.appendChild(results);
+    console.log(sel);
+    console.log("option created!");
+};
+
+
 // what to do when we received request
 var responseHandler = function () {
     console.log("response text", this.responseText);
     var response = JSON.parse(this.responseText);
-    //console.log(response);
+    console.log(response);
 
     //console.log(response[0]);
 
     // after receiving response,
-    // store each result show.id as value for <option>
-    var showId = response[0].show.id;
+    // retrieve all the results based on search input
+    for (var i = 0; i < response.length; i++) {
+        // store each result show.id as value for <option>
+        showId = response[i].show.id;
 
-    // store each result show.name as innerHTML for <option> & <div><h1>
-    var showName = response[0].show.name;
+        // store each result show.name as innerHTML for <option> & <div><h1>
+        showName = response[i].show.name;
 
-    // store each result show.image.original as img src for <div><img>
-    var showImg = response[0].show.image.original;
+        // store each result show.image.original as img src for <div><img>
+        if (response[i].show.image !== null) {
+         showImg = response[i].show.image.original;
+        } else {
+            showImg = "";
+        }
 
-    // store each result show.summary as innerHTML for <div><p>
-    var showSum = response[0].show.summary;
+        // store each result show.summary as innerHTML for <div><p>
+        showSum = response[i].show.summary;
 
+        addShowtoList();
+    }
     // for (var i = 0; i < response.length; i++) {
     //     console.log(response[i].show.name);
     // }
 
-    console.log(showId);
-    console.log(showName);
-    console.log(showImg);
-    console.log(showSum);
-
 };
+
 
 
 // make a new request
@@ -45,3 +79,9 @@ request.open("GET", "http://api.tvmaze.com/search/shows?q=boys");
 
 // send the request
 request.send();
+
+// create dropdown list when window load
+// var onClickSel = function () {
+//     addShowtoList();
+//     console.log("show added");
+// }
