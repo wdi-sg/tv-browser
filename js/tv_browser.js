197a5692819a3d300cc44bfd7bbdf9afca5d475a
EliseChia <swedesurrender@gmail.com>
2019-07-05 01:06:52 +0800

    rough code for individual show selection, testing

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,4 +1,5 @@
 var submitButton = document.getElementById("submit")
+var dropDownDisplay = document.getElementById("show-select");
 
 var getResults = function() {
     var searchBar = document.getElementById("search-bar");
@@ -16,7 +17,6 @@ var getResults = function() {
             console.log(results);
 
             //perform for loop on parsed results and use values to populate drop down
-            var dropDownDisplay = document.getElementById("show-select");
             var numOfResults = Object.keys(results).length;
 
             for (var i = 0; i < numOfResults; i++) {
@@ -36,3 +36,42 @@ var getResults = function() {
 };
 
 submitButton.addEventListener("click", getResults);
+
+//Attach an event listener to the select (NOT the option). When the user selects an option make another AJAX call. Use the response of that AJAX call to render the response of the individual show and display it in the output.
+
+var displaySelectedShow = function() {
+    // grab the value of that selected option >
+    var selectedShow = dropDownDisplay.options[dropDownDisplay.selectedIndex].value;
+
+    // selectedShow becomes the new query. i think can reuse endpoint variable. if not then change lor
+    var endpoint= `http://api.tvmaze.com/singlesearch/shows?q=${selectedShow}`;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", endpoint, true);
+
+    //this will return a SINGLE object with several keys (some are nested)
+    xhr.onload = function() {
+        if (this.status === 200) {
+            //check to make sure request was successful
+            console.log(this.responseText);
+            var results = JSON.parse(this.responseText);
+            console.log(results);
+
+            var output= document.getElementById("show-detail");
+
+            var showImage = document.createElement("img");
+            showImage.src = results.image.original;
+            output.appendChild(showImage);
+
+            var showTitle = document.createElement("h1");
+            showTitle.innerHTML = results.name;
+            output.appendChild(showTitle);
+
+            var showDescription= document.createElement("p");
+            showDescription.innerHTML = results.summary;
+            output.appendChild(showDescription);
+        }
+    }
+};
+
+dropDownDisplay.addEventListener("change", displaySelectedShow);
