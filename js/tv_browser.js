1c63a5dea529882613e1db6cb79cc9317e4e2b90
kach92 <kenny.ang9206@gmail.com>
2019-07-04 20:37:06 +0800

    finish up to full app, as similar as the sample given. going to stop here and think what to do for project...

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -7,7 +7,7 @@ var userInput = document.getElementById("show-search");
 var urlHome = "http://api.tvmaze.com/search/shows?q=";
 var urlSingle = "http://api.tvmaze.com/singlesearch/shows?q="
 var selectBar = document.getElementById("show-select");
-
+var detailDisplay = document.getElementById("show-detail");
 
 
 
@@ -19,6 +19,14 @@ var requestHappened = function(){
 
 //create options
 var makeOptions = function(arr){
+    //create the first options
+    clearOptions();
+    var createFirstOption = document.createElement("option");
+    createFirstOption.setAttribute("value","");
+    createFirstOption.innerHTML="Shows matching "+userInput.value+"...";
+    selectBar.appendChild(createFirstOption);
+
+    //create result options
     for(i=0;i<arr.length;i++){
             var createOption = document.createElement("option");
             createOption.innerHTML=arr[i].show.name;
@@ -26,19 +34,49 @@ var makeOptions = function(arr){
         }
 }
 
+//when an option is selected
 var secondRequestHappened = function(){
     var myObject = JSON.parse(this.responseText);
-
+    display(myObject);
 }
 
 //display search result
-var display = function(str){
-    document.getElementById("show-detail").innerHTML=str;
+var display = function(obj){
+    clearDisplay();
+
+    var createTitle = document.createElement("h2");
+    createTitle.innerHTML = obj.name;
+    var createImg = document.createElement("img");
+    createImg.setAttribute("src",obj.image.medium);
+
+    detailDisplay.appendChild(createTitle);
+    detailDisplay.appendChild(createImg);
+    detailDisplay.innerHTML += obj.summary;
+}
+
+//clear details display
+var clearDisplay = function(){
+    var child = detailDisplay.lastElementChild;
+    while (child) {
+        detailDisplay.removeChild(child);
+        child = detailDisplay.lastElementChild;
+    }
+
+}
+
+//clear the options field
+var clearOptions = function(){
+    var child = selectBar.lastElementChild;
+    while (child) {
+        selectBar.removeChild(child);
+        child = selectBar.lastElementChild;
+    }
 }
 
 //when submit button is clicked, send request
 submitButton.addEventListener("click",function(){
 event.preventDefault();
+    selectBar.style.visibility = "visible";
     var request = new XMLHttpRequest();
     request.addEventListener("load", requestHappened);
     var endpoint = `${urlHome}${userInput.value}`;
