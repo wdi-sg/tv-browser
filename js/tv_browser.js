fd3f6a5d27df79743ae008dc11fc3762299bbab9
AQILAHRAJAB <AQILAH@MAJULAH.SG>
2019-07-04 19:20:44 +0800

    TV-BROWSER - Manage to appear in the console.log

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,32 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+
+console.log("javascript works");
+
+var inputField = document.querySelector("#show-search");
+var button = document.querySelector("#submitButton");
+var storeData = "";
+
+var getInput = function () {
+    storeData = inputField.value;
+
+    var responseHandler = function () {
+        var myObject = JSON.parse(this.responseText);
+        console.log(myObject);
+        // var height = myObject["height"];
+        // console.log("Data: Height: "+height);
+
+            for (var i=0; i< myObject.length; i++) {
+            var name = myObject[i]["show"]["name"];
+            console.log(name);
+            }
+        }
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", responseHandler);
+    request.open("GET", "http://api.tvmaze.com/search/shows?q=" + storeData);
+    request.send();
+
+};
+
+button.addEventListener('click', getInput);
