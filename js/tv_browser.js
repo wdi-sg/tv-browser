dddf565f45482775dbb2a1fea18d7b8eeea46632
Jonathan <jonathanbek93@gmail.com>
2019-07-05 00:11:52 +0800

    finished first two steps, unable to make the second AJAX call from options

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,33 +1,13 @@
-// API Docs at:
-// http://www.tvmaze.com/api
-// Assignment:
-// use the input to get the search term from user, and display results in the DOM
-// further:
-// give user an option element for each result(eg. when girls is typed in, there are many options containing  girls)
-// attach an event listener to the select so that when they click on these options the indiv show displays
-// images: just link to them
-// search: use show search
-// for the event listener use single search?
 var responseHandler = function(){
-console.log("response text", this.responseText);
+// console.log("response text", this.responseText);
 // console.log("status text", this.statusText);
 // console.log("status code", this.status);
 var response = JSON.parse(this.responseText);
 console.log("response is :" + response);
+createOpt(response); //creates and appends options to select
+clickOpt();  //clickifies options
+console.log(sel);
 };
-
-
-// var request = new XMLHttpRequest();
-
-// request.addEventListener("load", responseHandler);
-
-// request.open("GET", "http://api.tvmaze.com/search/shows?q=girls");
-
-// request.send();
-
-var showSearch = "http://api.tvmaze.com/search/shows?q=";
-var searchTerm = "";
-document.getElementById('submit').addEventListener('click', goSubmit);
 
 var goSubmit = function(event){
     console.log("goSubmit activated");
@@ -39,8 +19,50 @@ var goSubmit = function(event){
 
     request.addEventListener("load", responseHandler);
 
-    searchTerm = `${showSearch}${url}`;
+    searchTerm = showSearch + url;
     request.open("GET", searchTerm);
 
     request.send();
+}
+
+var showSearch = "http://api.tvmaze.com/search/shows?q=";
+var singleSearch = "http://api.tvmaze.com/singlesearch/shows?q="
+var searchTerm = "";
+var submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', goSubmit);
+
+
+
+var sel = document.getElementById('show-select');
+
+
+var createOpt = function(response){
+    for (i = 0; i < response.length; i += 1){
+        var opt = document.createElement('option');
+        opt.innerHTML = response[i].show.name;
+        opt.setAttribute = "id, `opt${i}`";
+        sel.appendChild(opt);
+    }
+
+}
+
+var getShow = function(event){
+        var request = new XMLHttpRequest();
+
+        request.addEventListener("load", responseHandler);
+
+        searchTerm = singleSearch + event.target.innerHTML;
+        request.open("GET", searchTerm);
+
+        request.send();
+        console.log( response )
+
+    console.log("searchterm :" + searchTerm);
+}
+
+var clickOpt = function(){
+    for (j = 1; j < sel.length; j += 1){
+        console.log("options clickified!")
+        sel[j].addEventListener('change', getShow);
+    }
 }
