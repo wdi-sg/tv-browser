e5f63ffdb2a286b5c9e815fe78e702d03be2b964
sharkyu3 <lowhuiyu@hotmail.com>
2019-07-05 08:36:09 +0800

    started assignment. not able to get search field to work

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,25 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+
+var doSubmit = function(event){
+    var input = document.querySelector('#show-search');
+    var searchField = input.value;
+
+
+    // make a new request
+var request = new XMLHttpRequest();
+
+// listen for the request response
+request.addEventListener("load", responseHandler);
+
+
+// ready the system by calling open, and specifying the url
+request.open("GET", url);
+
+// send the request
+request.send();
+
+}
+
+document.querySelector('button').addEventListener('click', doSubmit);
