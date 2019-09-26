fe03cc48479de43b12a469abaa41103ec54c341d
SYNG <simyenng@gmail.com>
2019-09-27 01:23:02 +0800

    Completed Furthers

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,48 +1,75 @@
 // API Docs at:
 // http://www.tvmaze.com/api
 
+// to display final selected
+function displaySelected() {
+    console.log("response text", this.responseText);
+    var response = JSON.parse( this.responseText );
+    console.log( response );
+
+    // DOM final results
+    var displayAt = document.querySelector('#show-detail');
+
+    var title = document.createElement("h1");
+    title.innerHTML = response.name;
+    displayAt.appendChild(title);
+
+    var image = document.createElement("img");
+    image.src = response.image.medium;
+    displayAt.appendChild(image);
+
+    var summary = document.createElement("p");
+    summary.innerHTML = response.summary;
+    displayAt.appendChild(summary);
+
+}
+
+// to request result of selected title
+function getResult() {
+
+    //search again by Show ID
+    var showId = this.value;
+    console.log("Show ID is: " + showId);
+
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", displaySelected);
+    request.open("GET", `http://api.tvmaze.com/shows/${showId}`);
+    request.send();
+
+}
+
 var responseHandler = function() {
   console.log("response text", this.responseText);
   var response = JSON.parse( this.responseText );
   console.log( response );
 
   // //display search results
+  // var displayAt = document.querySelector('#show-detail');
   // var display = document.createElement("div");
-  // display.id = "results";
   // display.innerHTML = JSON.stringify(response);
-  // document.body.appendChild(display);
+  // displayAt.appendChild(display);
 
+  //this returns a HTML Object
+  var toReplace = document.getElementsByTagName("option");
+  console.log(toReplace);
+  //first option is [0]
+  toReplace[0].innerHTML = "Shows matching " + document.querySelector('#show-search').value;
 
-    var input = document.querySelector('#show-search');
-    var subj = input.value;
+  var select = document.querySelector('#show-select');
 
-    var select = document.querySelector('#show-select');
-    // var toRemove = document.getElementsByTagName("option");
-    // console.log(toRemove);
-    // select.removeChild(toRemove);
-
-    var matching = document.createElement("option");
-    matching.value = "";
-    matching.innerHTML = "Shows matching " + document.querySelector('#show-search').value;
-    select.appendChild(matching);
-
-  //add to options
+  // add options
   for ( i=0; i < response.length; i++ ) {
-    var show = response[i].show.name;
+    var show = response[i].show;
 
     var result = document.createElement("option");
-    result.innerHTML = show;
-    result.id = "option"+i;
+    result.innerHTML = show.name;
+    // append Show ID to search if selected
+    result.value = show.id;
     select.appendChild(result);
   }
 }
 
-// var requestFailed = function(){
-//     console.log("response text", this.responseText);
-//     console.log("status text", this.statusText);
-//     console.log("status code", this.status);
-// };
-
+// start search on submit
 var doSubmit = function(event){
 
     var input = document.querySelector('#show-search');
@@ -54,8 +81,6 @@ var doSubmit = function(event){
     // listen for the request response
     request.addEventListener("load", responseHandler);
 
-    // request.addEventListener("error", requestFailed);
-
     // ready the system by calling open, and specifying the url
     request.open("GET", `http://api.tvmaze.com/search/shows?q=${subj}`);
 
@@ -65,40 +90,11 @@ var doSubmit = function(event){
 
 //to start search
 document.querySelector('#submit').addEventListener('click', doSubmit);
-
-
-//to display final selected
-function displaySelected() {
-    console.log("response text", this.responseText);
-    var response = JSON.parse( this.responseText );
-    console.log( response );
-
-//DOM final results
-    var displayAt = document.querySelector('#show-detail');
-
-    var title = document.createElement("h1");
-    title.innerHTML = document.querySelector('#show-select').value;
-    displayAt.appendChild(title);
-
-}
-
-
-function getResult() {
-//    var selected = this.innerHTML
-    var selected = document.querySelector('#show-select').value;
-    console.log(selected);
-
-    var request = new XMLHttpRequest();
-    request.addEventListener("load", displaySelected);
-    request.open("GET", `http://api.tvmaze.com/search/shows?q=${selected}`);
-    request.send();
-
-}
-
+//to get selected show
 document.querySelector('#show-select').addEventListener('change', getResult);
 
 //to clear displayed results
 function clearDisplay() {
-  var toClear = document.getElementById("results");
+  var toClear = document.querySelector('#show-detail');
   toClear.innerHTML = "";
 }
