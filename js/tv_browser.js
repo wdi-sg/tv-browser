829e8b765dc4bf177a4e1402db9c726c46a1ec86
kevinngth <kevin.ng.th@gmail.com>
2019-09-26 16:38:59 +0800

    MVP done

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,10 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+const submitClick = (event) => {
+    let input = document.querySelector('#show-search')
+    console.log("i'm activated!!!");
+    let searchTerm = input.value;
+    document.querySelector('#searchTerm').innerText = searchTerm;
+}
+
+document.querySelector('#submit').addEventListener('click', submitClick);
