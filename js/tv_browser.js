575e123620da5dd8b711a81f9dbc4d7dfd54bd77
ShaneC <shanes_email_is@yahoo.com>
2019-09-27 05:01:06 +0800

    Added search results after user inputs.

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,14 +1,24 @@
 // API Docs at:http://api.tvmaze.com/search/shows?q=girls
 // http://www.tvmaze.com/api
-
+var showContainer = document.getElementById("show-detail");
 var btn = document.getElementById("btn");
 
 btn.addEventListener('click', function() {
-   ar userRequest = new XMLHttpRequest();
+   var userRequest = new XMLHttpRequest();
 userRequest.open('GET', 'http://api.tvmaze.com/search/shows?q=girls');
 userRequest.onload = function() {
     var userInfo = JSON.parse(userRequest.responseText);
-    console.log(userInfo[0]);
+    renderHTML(userInfo)
 };
 userRequest.send();
 })
+
+function renderHTML(data) {
+
+    var htmlString = "";
+
+    for(i=0; i < data.length; i++)
+        htmlString += "<p>" + data[i].show.name + " will be screened in " + data[i].show.language + " with a runtime of " + data[i].show.runtime + " min.</p>";
+    showContainer.insertAdjacentHTML('beforeend', htmlString);
+
+}
