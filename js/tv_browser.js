d6a47bdc9c59b0335201684ab3aee5ca58e018f8
SYNG <simyenng@gmail.com>
2019-09-26 21:14:00 +0800

    To finish up rest of DOM display

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -6,17 +6,30 @@ var responseHandler = function() {
   var response = JSON.parse( this.responseText );
   console.log( response );
 
-  //display results
-  var display = document.createElement("div");
-  display.id = "results";
-  display.innerHTML = JSON.stringify(response);
-  document.body.appendChild(display);
+  // //display search results
+  // var display = document.createElement("div");
+  // display.id = "results";
+  // display.innerHTML = JSON.stringify(response);
+  // document.body.appendChild(display);
+
+
+    var input = document.querySelector('#show-search');
+    var subj = input.value;
+
+    var select = document.querySelector('#show-select');
+    // var toRemove = document.getElementsByTagName("option");
+    // console.log(toRemove);
+    // select.removeChild(toRemove);
+
+    var matching = document.createElement("option");
+    matching.value = "";
+    matching.innerHTML = "Shows matching " + document.querySelector('#show-search').value;
+    select.appendChild(matching);
 
   //add to options
   for ( i=0; i < response.length; i++ ) {
     var show = response[i].show.name;
 
-    var select = document.querySelector('#show-select');
     var result = document.createElement("option");
     result.innerHTML = show;
     result.id = "option"+i;
@@ -24,19 +37,11 @@ var responseHandler = function() {
   }
 }
 
-
-
 // var requestFailed = function(){
 //     console.log("response text", this.responseText);
 //     console.log("status text", this.statusText);
 //     console.log("status code", this.status);
 // };
-
-//to clear displayed results
-function clearDisplay() {
-  var toClear = document.getElementById("results");
-  toClear.innerHTML = "";
-}
 
 var doSubmit = function(event){
 
@@ -56,5 +61,44 @@ var doSubmit = function(event){
 
     // send the request
     request.send();
-};
+}
+
+//to start search
 document.querySelector('#submit').addEventListener('click', doSubmit);
+
+
+//to display final selected
+function displaySelected() {
+    console.log("response text", this.responseText);
+    var response = JSON.parse( this.responseText );
+    console.log( response );
+
+//DOM final results
+    var displayAt = document.querySelector('#show-detail');
+
+    var title = document.createElement("h1");
+    title.innerHTML = document.querySelector('#show-select').value;
+    displayAt.appendChild(title);
+
+}
+
+
+function getResult() {
+//    var selected = this.innerHTML
+    var selected = document.querySelector('#show-select').value;
+    console.log(selected);
+
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", displaySelected);
+    request.open("GET", `http://api.tvmaze.com/search/shows?q=${selected}`);
+    request.send();
+
+}
+
+document.querySelector('#show-select').addEventListener('change', getResult);
+
+//to clear displayed results
+function clearDisplay() {
+  var toClear = document.getElementById("results");
+  toClear.innerHTML = "";
+}
