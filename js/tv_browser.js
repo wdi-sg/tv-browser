bb66dc30f35341860d272149af28768c80acd9b6
ShaneC <shanes_email_is@yahoo.com>
2019-09-27 04:16:16 +0800

    First step of adding JSON parse.

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,9 @@
-// API Docs at:
+// API Docs at:http://api.tvmaze.com/search/shows?q=girls
 // http://www.tvmaze.com/api
+var userRequest = new XMLHttpRequest();
+userRequest.open('GET', 'http://api.tvmaze.com/search/shows?q=girls');
+userRequest.onload = function() {
+    var userInfo = JSON.parse(userRequest.responseText);
+    console.log(userInfo[0]);
+};
+userRequest.send();
