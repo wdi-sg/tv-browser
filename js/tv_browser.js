3cca4214c365e2c9ce6a310f3839d4b0d5b28db6
kevinngth <kevin.ng.th@gmail.com>
2019-09-26 16:48:53 +0800

    done further 1

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -2,9 +2,15 @@
 // http://www.tvmaze.com/api
 const submitClick = (event) => {
     let input = document.querySelector('#show-search')
-    console.log("i'm activated!!!");
     let searchTerm = input.value;
     document.querySelector('#searchTerm').innerText = searchTerm;
+    let newOption = document.createElement('option');
+    let showSelect = document.querySelector('#show-select');
+    newOption.setAttribute('value', searchTerm);
+    newOption.innerText = searchTerm;
+    showSelect.append(newOption);
 }
 
 document.querySelector('#submit').addEventListener('click', submitClick);
+
+console.log(searchTerm);
