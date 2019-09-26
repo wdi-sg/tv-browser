d6e38df935f6206024a87e98a52e5427a62bc915
kevinngth <kevin.ng.th@gmail.com>
2019-09-26 18:27:42 +0800

    done until Full app

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -4,6 +4,8 @@ const submitClick = (event) => {
     let input = document.querySelector('#show-search')
     let searchTerm = input.value;
     ajaxFunction(searchTerm)
+    document.querySelector('#firstChild').innerText = `Shows matching '${searchTerm}'...`
+    document.querySelector('#show-select').style.visibility = 'visible';
     input.value = "";
 }
 
@@ -15,7 +17,6 @@ const ajaxFunction = (value) => {
     console.log(event.target.value)
     request.open("GET", `http://api.tvmaze.com/search/shows?q=${value}`);
     request.send();
-    console.log("WEWEWEW");
 }
 
 const listAjax = (event) => {
@@ -27,29 +28,33 @@ const listAjax = (event) => {
 }
 
 var responseHandler = function() {
-  console.log("response text", this.responseText);
-  console.log("status text", this.statusText);
-  console.log("status code", this.status);
-  mazeArray = JSON.parse(this.responseText);
-  for (let i=0; i<mazeArray.length; i++) {
-    let optionName = document.createElement('option');
-    optionName.innerText = mazeArray[i].show.name;
-    optionName.setAttribute('value', mazeArray[i].show.name);
-    document.querySelector('#show-select').append(optionName);
+    console.log("response text", this.responseText);
+    console.log("status text", this.statusText);
+    console.log("status code", this.status);
+    mazeArray = JSON.parse(this.responseText);
+    for (let i=0; i<mazeArray.length; i++) {
+        let optionName = document.createElement('option');
+        optionName.innerText = mazeArray[i].show.name;
+        optionName.setAttribute('value', mazeArray[i].show.name);
+        document.querySelector('#show-select').append(optionName);
   }
 };
 
 var listHandler = function() {
-  console.log("response text", this.responseText);
-  console.log("status text", this.statusText);
-  console.log("status code", this.status);
-  // mazeArray = JSON.parse(this.responseText);
-  // for (let i=0; i<mazeArray.length; i++) {
-  //   let optionName = document.createElement('option');
-  //   optionName.innerText = mazeArray[i].show.name;
-  //   optionName.setAttribute('value', mazeArray[i].show.name);
-  //   document.querySelector('#show-select').append(optionName);
-  // }
+    let currentList = document.querySelector('#show-select')
+    while (currentList.firstChild) {
+        currentList.removeChild(currentList.firstChild);
+    }
+    console.log("response text", this.responseText);
+    console.log("status text", this.statusText);
+    console.log("status code", this.status);
+    let array = JSON.parse(this.responseText);
+        for (let i=0; i<array.length; i++) {
+        let optionName = document.createElement('option');
+        optionName.innerText = array[i].show.name;
+        optionName.setAttribute('value', array[i].show.name);
+        document.querySelector('#show-select').append(optionName);
+  }
 };
 
 document.querySelector('#show-select').addEventListener("change", listAjax);
