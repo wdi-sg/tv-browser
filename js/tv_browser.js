4b8d4088599a8049c02dfd8dcb2b4e75910c930c
ShaneC <shanes_email_is@yahoo.com>
2019-09-27 04:24:55 +0800

    Added eventListener to button!

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,9 +1,14 @@
 // API Docs at:http://api.tvmaze.com/search/shows?q=girls
 // http://www.tvmaze.com/api
-var userRequest = new XMLHttpRequest();
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener('click', function() {
+   ar userRequest = new XMLHttpRequest();
 userRequest.open('GET', 'http://api.tvmaze.com/search/shows?q=girls');
 userRequest.onload = function() {
     var userInfo = JSON.parse(userRequest.responseText);
     console.log(userInfo[0]);
 };
 userRequest.send();
+})
