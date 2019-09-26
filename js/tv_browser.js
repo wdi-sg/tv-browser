d91d7bdf521d336dc1170d5ca1d9fbea89e6d713
kevinngth <kevin.ng.th@gmail.com>
2019-09-26 18:09:48 +0800

    finished further 2

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,16 +1,55 @@
-// API Docs at:
-// http://www.tvmaze.com/api
+let mazemazeArray = [];
+
 const submitClick = (event) => {
     let input = document.querySelector('#show-search')
     let searchTerm = input.value;
-    document.querySelector('#searchTerm').innerText = searchTerm;
-    let newOption = document.createElement('option');
-    let showSelect = document.querySelector('#show-select');
-    newOption.setAttribute('value', searchTerm);
-    newOption.innerText = searchTerm;
-    showSelect.append(newOption);
+    ajaxFunction(searchTerm)
+    input.value = "";
 }
 
 document.querySelector('#submit').addEventListener('click', submitClick);
 
-console.log(searchTerm);
+const ajaxFunction = (value) => {
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", responseHandler);
+    console.log(event.target.value)
+    request.open("GET", `http://api.tvmaze.com/search/shows?q=${value}`);
+    request.send();
+    console.log("WEWEWEW");
+}
+
+const listAjax = (event) => {
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", listHandler);
+    console.log(event.target.value)
+    request.open("GET", `http://api.tvmaze.com/search/shows?q=${event.target.value}`);
+    request.send();
+}
+
+var responseHandler = function() {
+  console.log("response text", this.responseText);
+  console.log("status text", this.statusText);
+  console.log("status code", this.status);
+  mazeArray = JSON.parse(this.responseText);
+  for (let i=0; i<mazeArray.length; i++) {
+    let optionName = document.createElement('option');
+    optionName.innerText = mazeArray[i].show.name;
+    optionName.setAttribute('value', mazeArray[i].show.name);
+    document.querySelector('#show-select').append(optionName);
+  }
+};
+
+var listHandler = function() {
+  console.log("response text", this.responseText);
+  console.log("status text", this.statusText);
+  console.log("status code", this.status);
+  // mazeArray = JSON.parse(this.responseText);
+  // for (let i=0; i<mazeArray.length; i++) {
+  //   let optionName = document.createElement('option');
+  //   optionName.innerText = mazeArray[i].show.name;
+  //   optionName.setAttribute('value', mazeArray[i].show.name);
+  //   document.querySelector('#show-select').append(optionName);
+  // }
+};
+
+document.querySelector('#show-select').addEventListener("change", listAjax);
