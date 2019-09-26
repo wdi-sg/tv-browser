721c4c9c2e641cf708c0bdaa50ae6ebbe878d76f
ShaneC <shanes_email_is@yahoo.com>
2019-09-27 06:30:45 +0800

    Search results displayed.

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,10 +1,16 @@
-// API Docs at:http://api.tvmaze.com/search/shows?q=girls
+// API Docs at:
 // http://www.tvmaze.com/api
+
+
 var showContainer = document.getElementById("show-detail");
 var btn = document.getElementById("btn");
 
+
 btn.addEventListener('click', function() {
+
    var userRequest = new XMLHttpRequest();
+
+
 userRequest.open('GET', 'http://api.tvmaze.com/search/shows?q=girls');
 userRequest.onload = function() {
     var userInfo = JSON.parse(userRequest.responseText);
@@ -20,5 +26,4 @@ function renderHTML(data) {
     for(i=0; i < data.length; i++)
         htmlString += "<p>" + data[i].show.name + " will be screened in " + data[i].show.language + " with a runtime of " + data[i].show.runtime + " min.</p>";
     showContainer.insertAdjacentHTML('beforeend', htmlString);
-
-}
+    };
