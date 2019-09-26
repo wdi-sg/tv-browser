83bb49b5fdba75aaa65ec7c168a5c7048a51f41e
SYNG <simyenng@gmail.com>
2019-09-26 17:52:10 +0800

    Search enabled

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,34 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+var responseHandler = function() {
+  console.log("response text", this.responseText);
+  console.log("status text", this.statusText);
+  console.log("status code", this.status);
+};
+
+// var requestFailed = function(){
+//     console.log("response text", this.responseText);
+//     console.log("status text", this.statusText);
+//     console.log("status code", this.status);
+// };
+
+var doSubmit = function(event){
+    var input = document.querySelector('#show-search');
+    var subj = input.value;
+
+    // make a new request
+    var request = new XMLHttpRequest();
+
+    // listen for the request response
+    request.addEventListener("load", responseHandler);
+
+    // request.addEventListener("error", requestFailed);
+
+    // ready the system by calling open, and specifying the url
+    request.open("GET", `http://api.tvmaze.com/search/shows?q=${subj}`);
+
+    // send the request
+    request.send();
+};
+document.querySelector('#submit').addEventListener('click', doSubmit);
