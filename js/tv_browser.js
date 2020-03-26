fe4a8c6390478c12fd4c53916721a29d629183aa
chelsejw <chelsejw@gmail.com>
2020-03-26 18:54:10 +0800

    add returns multiple results function

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -6,7 +6,7 @@ var request = new XMLHttpRequest();
 var display = document.getElementById('show-detail')
 const inputElement = document.getElementById('show-search');
 const submitBtn = document.getElementById('submit-btn');
-const url = `http://api.tvmaze.com/singlesearch/shows?q=`
+const url = `http://api.tvmaze.com/search/shows?q=`
 
 submitBtn.addEventListener('click', submitRequest)
 
@@ -14,14 +14,14 @@ submitBtn.addEventListener('click', submitRequest)
 var responseHandler = function() {
     console.log(`responseHandler triggered`)
     var results = JSON.parse(this.responseText);
-    console.log(`Results are ${results}`);
-    var itemName = document.createElement("h1");
-    var itemDesc = document.createElement("p");
-    itemName.innerText = results.name;
-    itemDesc.innerHTML = results.summary;
-    display.appendChild(itemName);
-    display.appendChild(itemDesc);
-
+    for (var index=0; index < results.length; index++) {
+      var itemName = document.createElement("h1");
+      var itemDesc = document.createElement("p");
+      itemName.innerText = results[index].show.name;
+      itemDesc.innerHTML = results[index].show.summary;
+      display.appendChild(itemName);
+      display.appendChild(itemDesc);
+    }
 };
 
 var requestFailed = function() {
