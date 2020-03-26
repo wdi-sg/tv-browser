b7cbb6ae3684358995333ae557962419007adeea
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-03-26 22:53:56 +0800

    Options show suggestions for tvshows based on user input

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,14 +1,38 @@
 // API Docs at:
 // http://www.tvmaze.com/api
 
-const userInput = document.getElementById('show-search');
 
-const getInput = (event) => {
-    const main = document.querySelector('main');
-    const showInput = document.createElement('p');
-    showInput.textContent = userInput.value;
-    main.appendChild(showInput);
+var getInput = function(){
+    // Get userInput
+    const userInput = document.getElementById('show-search').value;
+    const userInputLower = userInput.toLowerCase();
+
+    // Ready the system by calling open and specifying URL
+    request.open("GET", `http://api.tvmaze.com/search/shows?q=${userInputLower}`);
+
+    // Send the request
+    request.send();
 }
 
 const button = document.querySelector('button');
 button.addEventListener('click', getInput);
+
+// Make a new request
+var request = new XMLHttpRequest();
+
+var responseHandler = function(){
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+
+    //Show in Options
+    const showSelect = document.getElementById('show-select');
+    for(let i=0; i<response.length; i++){
+        const option = document.createElement('option');
+        option.value = "";
+        option.textContent = response[i]["show"].name;
+        showSelect.appendChild(option);
+    }
+}
+
+// Listen for the request response
+request.addEventListener("load", responseHandler);
