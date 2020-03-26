bb36630c069faa82762537fc1e8ce881dec98e28
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-03-27 00:23:13 +0800

    User able to choose option which renders it onto the current page

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -8,7 +8,11 @@ var getInput = function(){
     const userInputLower = userInput.toLowerCase();
 
     // Ready the system by calling open and specifying URL
+    // request.open("GET", `http://api.tvmaze.com/search/shows?q=${userInputLower}`);
     request.open("GET", `http://api.tvmaze.com/search/shows?q=${userInputLower}`);
+
+    // Listen for the request response
+    request.addEventListener("load", responseHandler);
 
     // Send the request
     request.send();
@@ -20,19 +24,55 @@ button.addEventListener('click', getInput);
 // Make a new request
 var request = new XMLHttpRequest();
 
+var response;
+
 var responseHandler = function(){
-    var response = JSON.parse(this.responseText);
+    response = JSON.parse(this.responseText);
     console.log(response);
 
     //Show in Options
     const showSelect = document.getElementById('show-select');
     for(let i=0; i<response.length; i++){
         const option = document.createElement('option');
-        option.value = "";
+        option.value = i;
         option.textContent = response[i]["show"].name;
+        option.className = 'hello';
         showSelect.appendChild(option);
     }
+
+    showSelect.addEventListener('change', optionSelect);
 }
 
-// Listen for the request response
-request.addEventListener("load", responseHandler);
+
+
+
+
+
+//////////////////////////////////////////////////////////Select Option
+
+
+// Make a new request
+var showRequest = new XMLHttpRequest();
+
+var showResponseHandler = function(){
+    var show = document.createElement('div');
+    show.innerHTML = this.responseText;
+
+    const body = document.querySelector('body');
+    body.appendChild(show);
+}
+
+showRequest.addEventListener('load', showResponseHandler);
+
+var showSelectionNum;
+
+var optionSelect = function(event){
+    showSelectionNum = event.target.value;
+    var URL = response[showSelectionNum]["show"].url;
+
+    // get request
+    showRequest.open("GET", URL);
+
+    // Send request
+    showRequest.send();
+}
