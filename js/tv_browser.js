4ed91a1daa4f0c74b50de728c6ae828e55916528
dannst <arcanic@gmail.com>
2020-03-27 00:50:41 +0800

    basic functioning app

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,9 +1,15 @@
 // API Docs at:
 // http://www.tvmaze.com/api
 
+//AJAX
+request.addEventListener("load", responseHandler);
+request.open("GET", "http://api.tvmaze.com/search/shows?q=girls");
+request.send();
+
+
 let request = new XMLHttpRequest();
 let response = [];
-let user_input = document.querySelectorAll('input')[0].value;
+
 const ele_select = document.querySelectorAll('select')[0];
 const ele_button = document.querySelectorAll('button')[0];
 
@@ -13,6 +19,7 @@ const responseHandler = function(){
 };
 
 const inputHandler = function(){
+	let user_input = document.querySelectorAll('input')[0].value;
 	for (i = 0; i < response.length; i++){
 		if (response[i].show.name.toLowerCase().includes(user_input.toLowerCase())){
 			let ele_option = document.createElement('option');
@@ -30,14 +37,40 @@ const getURL = function(name_of_film){
 	};
 };
 
-//AJAX
-request.addEventListener("load", responseHandler);
-request.open("GET", "http://api.tvmaze.com/search/shows?q=girls");
-request.send();
+const getImage = function(name_of_film){
+	for (i = 0; i < response.length; i++){
+		if (response[i].show.name === name_of_film){
+			return response[i].show.image.medium;
+		};
+	};
+};
 
 //eventlisteners
-ele_button.addEventListener('click', inputHandler);
+ele_button.addEventListener('click', function(event){
+	let user_input = document.querySelectorAll('input')[0].value;
+	inputHandler();
+	ele_select.removeAttribute('id');
+	document.querySelector('option').innerHTML = `Shows matching "${user_input}"`;
+});
+
+
+
+
 ele_select.addEventListener('change', function(event){
 	responseHandler;
-	window.open(getURL(event.target.value));
+	let user_input = document.querySelectorAll('input')[0].value;
+	let image = document.createElement('img')
+		image.src = getImage(event.target.value);
+	document.querySelector('#show-detail').innerHTML = event.target.value;
+	document.querySelector('#show-detail').appendChild(image);
+
+	// window.open(getURL(event.target.value));
 });
+
+
+
+
+
+
+
+
