eb950adf6e2c61a3d9787bdfbbbb821b00b7f046
kokchuantan <kokchuan1996@gmail.com>
2020-03-27 00:28:56 +0800

    added some comments so i can understand better

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -8,8 +8,9 @@ var selectShow = document.getElementById('show-select');
 
 // run after submit button clicked
 var submitSearch = function () {
-    var request = new XMLHttpRequest();
+    //save value inside searchbar
     var userInput = searchShow.value;
+    //put saved value into query url
     var url = `http://api.tvmaze.com/search/shows?q=${userInput}`
     var responseHandler = function () {
         response = JSON.parse(this.responseText);
@@ -18,13 +19,17 @@ var submitSearch = function () {
         for (var x = 0; x < response.length; x++) {
             // print all search results into show details section
             var result = document.createElement('p');
+            // print show names for search results
             result.innerText = response[x].show.name;
+            // print names into body
             showDetails.appendChild(result)
             var options = document.createElement('option');
             options.innerText = response[x].show.name;
+            // print names into option bar
             selectShow.appendChild(options);
         }
     };
+    var request = new XMLHttpRequest();
 
     request.addEventListener("load", responseHandler);
 
