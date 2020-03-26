6f48e42dfe155e9f36957bce9e1c2df4185068f9
Dyana Wu <dyanawu@gmail.com>
2020-03-26 22:51:57 +0800

    Display title and image

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -2,8 +2,49 @@
 // http://www.tvmaze.com/api
 
 //fuzzy search api endpoint: http://api.tvmaze.com/search/shows?q=girls
-var searchResults;
 var pattern = "";
+
+displayShowInfo = function (showName, showImgSrc) {
+  if (document.querySelector("#show-title") === null) {
+    showDetails = document.querySelector("#show-detail");
+
+    var showTitle = document.createElement("h2");
+    showTitle.innerText = showName;
+    showTitle.id = "show-title";
+    var showPoster = document.createElement("img");
+    showPoster.src = showImgSrc;
+    showPoster.id = "show-poster";
+    showDetails.appendChild(showTitle);
+    showDetails.appendChild(showPoster);
+  } else {
+    console.log("here");
+    var oldShowTitle = document.querySelector("#show-title");
+    oldShowTitle.parentElement.removeChild(oldShowTitle);
+    var oldShowPoster = document.querySelector("#show-poster");
+    oldShowPoster.parentElement.removeChild(oldShowPoster);
+
+    displayShowInfo(showName, showImgSrc);
+  }
+}
+
+var parseShowInfo = function () {
+  var response = JSON.parse(this.responseText);
+  var showName = response.name;
+  var showImgSrc = (response.image === null) ? "images/204.jpeg" : response.image.medium;
+
+  displayShowInfo(showName, showImgSrc);
+}
+
+var fetchShowInfo = function () {
+  var showId = this.value;
+  var url = "http://api.tvmaze.com/shows/" + showId;
+
+  var request = new XMLHttpRequest;
+  request.addEventListener("load", parseShowInfo);
+  request.addEventListener("error", handleError);
+  request.open("GET", url);
+  request.send();
+}
 
 var displayShowList = function (shows) {
   var dropDown = document.querySelector("#show-select");
@@ -17,6 +58,7 @@ var displayShowList = function (shows) {
     dropDown.appendChild(optionItem);
   }
   dropDown.style.visibility = "visible";
+  dropDown.addEventListener("change", fetchShowInfo);
 }
 
 var buildShowList = function (searchResp) {
@@ -30,9 +72,9 @@ var buildShowList = function (searchResp) {
   displayShowList(showList);
 }
 
-var parseResponse = function () {
+var parseShowSearch = function () {
   var responseText = this.responseText;
-  searchResults = JSON.parse(responseText);
+  var searchResults = JSON.parse(responseText);
   buildShowList(searchResults);
 }
 
@@ -42,7 +84,7 @@ var handleError = function () {
   console.log("error code: ", this.status);
 }
 
-var submitSearch = function () {
+var fetchSearchResults = function () {
   var searchStr = document.querySelector("#show-search").value;
   pattern = searchStr;
   searchStr = searchStr.replace(" ", "+");
@@ -51,7 +93,7 @@ var submitSearch = function () {
   var url = "http://api.tvmaze.com/search/shows" + param;
 
   var request = new XMLHttpRequest;
-  request.addEventListener("load", parseResponse);
+  request.addEventListener("load", parseShowSearch);
   request.addEventListener("error", handleError);
   request.open("GET", url);
   request.send();
@@ -59,7 +101,7 @@ var submitSearch = function () {
 
 var setup = function () {
   var submitButton = document.querySelector("button");
-  submitButton.addEventListener("click", submitSearch);
+  submitButton.addEventListener("click", fetchSearchResults);
   var select = document.querySelector("#show-select");
   select.style.visibility = "hidden";
 }
