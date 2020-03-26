d38e1328787c9481e6bc6ba41bc1df014b204aae
wickker <choydianchun@gmail.com>
2020-03-26 18:49:39 +0800

    completed MVP and further part 1

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,38 @@
-// API Docs at:
-// http://www.tvmaze.com/api
+let searchedItem;
+let searchUrl;
+let showTitlesArr = [];
+let selectDiv = document.getElementById("show-select");
+
+var responseHandler = function() {
+  console.log("Response handler start");
+  // console.log("response text: ", this.responseText);
+  selectDiv.innerHTML = "";
+  let response = JSON.parse(this.responseText);
+  console.log(response);
+  console.log(response[0].show.name);
+  for (let i=0; i<response.length; i++) {
+    showTitlesArr.push(response[i].show.name);
+    let newOption = document.createElement("option");
+    newOption.textContent = response[i].show.name; 
+    selectDiv.appendChild(newOption);
+  }
+}
+
+var requestFailed = function() {
+  console.log("Request failed");
+};
+
+var doSubmit = function() {
+  var request = new XMLHttpRequest();
+  var input = document.getElementById("show-search");
+  searchedItem = input.value;
+  // console.log(searchedItem);
+  searchUrl = "http://api.tvmaze.com/search/shows?q=" + searchedItem;
+  request.open("GET", searchUrl);
+  request.addEventListener("load", responseHandler);
+  request.addEventListener("error", requestFailed);
+  request.send();
+};
+
+let button = document.getElementById("submitbutton");
+button.addEventListener("click", doSubmit);
