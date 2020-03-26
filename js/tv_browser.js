ddaa18f67ed592261c8f4dbb30cf186c2dc930f0
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-03-26 19:31:33 +0800

    first commit MVP

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,14 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+const userInput = document.getElementById('show-search');
+
+const getInput = (event) => {
+    const main = document.querySelector('main');
+    const showInput = document.createElement('p');
+    showInput.textContent = userInput.value;
+    main.appendChild(showInput);
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', getInput);
