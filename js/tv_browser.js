b202dd58b8662a9527f0b267829f9b6a8764e4af
jordanswp <sohweepin@gmail.com>
2020-03-26 22:00:27 +0800

    first changes to js

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,34 @@
-// API Docs at:
-// http://www.tvmaze.com/api
+var userInput = document.querySelector('#show-search');
+var button = document.querySelector('button');
+
+
+var responseHandler = function() {
+    console.log(this);
+    var responseText = this.responseText;
+    console.log(typeof responseText);
+    console.log("response text", this.responseText);
+    // var response = JSON.parse(responseText);
+    // response = JSON.stringify(response);
+    //console.log(response);
+    var searchResults = document.createElement('p');
+    searchResults.textContent = responseText;
+    var resultsDiv = document.getElementById("search-results");
+    resultsDiv.appendChild(searchResults);
+};
+
+var handleError = function(){};
+
+var userSubmit = function(event) {
+    var url = "http://api.tvmaze.com/search/shows?q=" + userInput.value;
+    console.log(url);
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", responseHandler);
+    request.addEventListener("error", handleError);
+    request.open("GET", url);
+
+    request.send();
+}
+
+
+
+button.addEventListener('click', userSubmit);
