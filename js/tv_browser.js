62d6fdbbefbb5f5195a485463d40c25251afb770
lekhweemeng <lek.hweemeng@live.com>
2020-03-27 01:19:05 +0800

    completed assignment on js script

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -3,8 +3,10 @@
 
 //setting the request var for use later
 var request = new XMLHttpRequest();
+var optionRequest = new XMLHttpRequest();
 var buttonGetter = document.getElementById('submit')
 var optionGetter = document.getElementById('show-select');
+var showDetailGetter = document.getElementById("show-detail");
 
 //setting the response handler for action
 var responseHandler = function() {
@@ -15,12 +17,23 @@ var responseHandler = function() {
     //looping to print all show names
     for (i=0; i<lengthOfArray; i++){
     var createOption = document.createElement('option');
-    createOption.value = response[i].show.name;
+    createOption.value = response[i].show.id;
     createOption.innerText = response[i].show.name;
     optionGetter.appendChild(createOption);
     }
 };
 
+var responseHandlerOpt = function() {
+    var response1 = JSON.parse( this.responseText );
+    console.log("status code", this.status);
+    console.log("response text", this.responseText);
+    var createShowNames = document.createElement('h1');
+    createShowNames.innerText = "Show Name: " + response1.name;
+    showDetailGetter.appendChild(createShowNames);
+    var createImage = document.createElement('img');
+    createImage.src = response1.image.original;
+    showDetailGetter.appendChild(createImage);
+}
 //setting up request api function.
 var requestAPI = function(){
     var input = document.querySelector('#show-search');
@@ -29,6 +42,15 @@ var requestAPI = function(){
     request.open("GET", "http://api.tvmaze.com/search/shows?q=" + entry);
     request.send();
 };
+
+// setting up the api request for the click on options.
+var optRequestApi = function (){
+    var idOfShow = optionGetter.value;
+    optionRequest.open("GET", "http://api.tvmaze.com/shows/" + idOfShow);
+    optionRequest.send();
+    console.log("status code", this.status);
+    console.log("response text", this.responseText);
+}
 
 
 
@@ -46,3 +68,7 @@ var requestAPI = function(){
 request.addEventListener("load", responseHandler);
 // setting, once submit button clicked then request for the api.
 buttonGetter.addEventListener('click', requestAPI);
+//setting once the item changed, get the value, which is the id and run optRequestApi
+optionGetter.addEventListener('change', optRequestApi);
+//defining the response handler for the loaded infomation upon chaning the value of the opton.
+optionRequest.addEventListener("load", responseHandlerOpt);
