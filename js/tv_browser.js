2b0ee14e6bdff97d418ded352cdad3dc102a5c0b
vigbit <vigneshb@live.com>
2020-03-27 15:06:22 +0800

    search by movie names

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,61 +1,38 @@
-// API Docs at:
-// http://www.tvmaze.com/api
 
-var results;
-  console.log(results);
-
-
-var showName = [];
-
-/*
-function getItems(objectData) {
-  return objectData["items"];
+var errorHandler = function(){
+    console.log(this)
 }
+var results;
 
-var findItem = function(){
-    for ( let i = 0; i < results.length; i ++){
-    var userEntry = document.querySelector('#show-search');
-    var searchWord = userEntry.value;
-    var show = getItems(movies);
-    console.log(show);
+var input = document.querySelector('#show-search');
+//console.log(results);
 
+var searchResult = function(){
+  var newArray = []
+    for( let i = 0; i < results.length; i ++){
+    var name = results[i].show.name;
+    newArray.push(name);
     }
-
-  }
-
-  /*var pTag = document.createElement('p');
-  pTag.innerText = response.name;
-  document.querySelector('#show-detail').appendChild(pTag);*/
+    return newArray;
+}
 
 //Response Handler*/
 var responseHandler = function() {
   console.log(this)
   console.log(this.responseText)
 
-  var response = JSON.parse(this.responseText);
-  console.log(Array.isArray(response))
-  console.log(response);
-
+var response = JSON.parse(this.responseText);
+  //console.log(Array.isArray(response))
+  //console.log(response);
   results = response;
-
-  console.log(findItem());
-
-  //var name = response.name
-  //console.log(name);
+  console.log(searchResult());
   // console.log
   console.log("response text", this.responseText);
   console.log("status text", this.statusText);
   console.log("status code", this.status);
-
-
 };
 
-var errorHandler = function(){
-    console.log(this)
-}
-
 var doSubmit = function(event){
-    var input = document.querySelector('#show-search');
     var url = input.value;
     // make a new request
     var request = new XMLHttpRequest();
@@ -69,3 +46,8 @@ var doSubmit = function(event){
 }
 
 document.querySelector('#submit').addEventListener('click', doSubmit);
+
+
+  /*var pTag = document.createElement('p');
+  pTag.innerText = response.name;
+  document.querySelector('#show-detail').appendChild(pTag);*/
